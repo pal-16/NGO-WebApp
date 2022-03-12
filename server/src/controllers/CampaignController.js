5ba49c1796e684601349767736499d84547ee237
PatkarAditya22 <adityapatkar22@gmail.com>
2022-03-13 00:39:55 +0530

    Nodemailer mails to nearby users for campaigns

--- server/src/controllers/CampaignController.js
+++ server/src/controllers/CampaignController.js
@@ -1,21 +1,81 @@
 const Campaign = require("../models/Campaign");
 const Organization = require("../models/Organization");
+const User = require("../models/User");
+const nodemailer = require("nodemailer");
+const GeocoderArcGIS = require("geocoder-arcgis");
+
+const geocoder = new GeocoderArcGIS();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "adityapatkar22@gmail.com",
+    pass: process.env.GMAIL_PASS
+  }
+});
 
 exports.createCampaign = async (req, res) => {
   try {
     console.log(req.body);
-    const { orgId, name, description, noOfVolunteers, time, date } = req.body;
+    const { orgId, name, description, noOfVolunteers, time, date, address } =
+      req.body;
+
+    let loc = [];
+    const result = await geocoder.findAddressCandidates(address, {});
+    loc.push(result.candidates[0].location.x);
+    loc.push(result.candidates[0].location.y);
+    let location = { type: "Point", coordinates: loc };
+
     const campaign = await Campaign.create({
       orgId,
       name,
       description,
       noOfVolunteers,
       time,
-      date
+      date,
+      location
     });
     await Organization.findByIdAndUpdate(orgId, {
       $push: { campaign: campaign }
     });
+
+    const result = await geocoder.findAddressCandidates(location, {});
+    const filteredUsers = await User.find({
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [
+              result.candidates[0].location.x,
+              result.candidates[0].location.y
+            ]
+          },
+          $maxDistance: range * 1000
+        }
+      }
+    });
+
+    const emails = await filteredUsers.map((user) => {
+      return user.email;
+    });
+    console.log(emails);
+
+    await emails.map((email) => {
+      var mailOptions = {
+        from: "adityapatkar22@gmail.com",
+        to: email,
+        subject: "Invitation to join the campaign",
+        text: "That was easy!"
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+    });
+
     res.status(201).json({
       status: "success",
       data: {}
--- server/src/models/Campaign.js
+++ server/src/models/Campaign.js
@@ -16,6 +16,21 @@ const CampaignSchema = new Schema(
       type: String,
       required: true
     },
+    address: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        default: "Point",
+        required: false
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    },
     noOfVolunteers: {
       type: Number,
       required: false
