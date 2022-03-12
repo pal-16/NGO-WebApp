3ae65dd59acde1d9a4695c507dc4c647d66c1432
PatkarAditya22 <adityapatkar22@gmail.com>
2022-03-13 01:41:22 +0530

    APIs

--- client/src/utils/Api/Campaign.js
+++ client/src/utils/Api/Campaign.js
@@ -1,17 +1,19 @@
 import axios from "./axios.js";
 
 const AuthApi = {
-	signIn: ({ email, password, userType }) => {
-		return axios.post(`/${userType}/login`, { email, password });
-	},
-	signUp: ({ email, password, name, address, userType }) => {
-		return axios.post(`/${userType}/register`, {
-			email,
-			password,
-			userType,
-			address,
-			name,
+	create: ({ orgId, title, description, totalAmount }) => {
+		return axios.post(`/campaign/create`, {
+			orgId,
+			title,
+			description,
+			totalAmount,
 		});
+	},
+	getAllPosts: () => {
+		return axios.get(`/crowdfunding/getAllCampaigns`);
+	},
+	getParticular: ({ orgId }) => {
+		return axios.get(`/org/campaign/${orgId}`);
 	},
 };
 
--- client/src/utils/Api/Crowdfunding.js
+++ client/src/utils/Api/Crowdfunding.js
@@ -1,18 +1,29 @@
 import axios from "./axios.js";
 
 const CrowdfundingApi = {
-	create: ({orgId,title,description, totalAmount }) => {
-		return axios.post(`/crowdfunding/create`, {orgId,title,description,totalAmount});
-    },
-	getAllPosts:()=>{
+	create: ({ orgId, title, description, totalAmount }) => {
+		return axios.post(`/crowdfunding/create`, {
+			orgId,
+			title,
+			description,
+			totalAmount,
+		});
+	},
+	getAllPosts: () => {
 		return axios.get(`/crowdfunding/getAllPosts`);
 	},
-	getParticular:({orgId})=>{
-		return axios.get(`/org/crowdfunding`+`${orgId}`);
+	getParticular: ({ orgId }) => {
+		return axios.get(`/org/crowdfunding/${orgId}`);
 	},
-	makeTransaction:({amount,postId,currOrgId,userId,paymentId})=>{
-		return axios.post(`/user/donate/`+paymentId,{amount,postId,currOrgId,userId,paymentId})
-	}
+	makeTransaction: ({ amount, postId, currOrgId, userId, paymentId }) => {
+		return axios.post(`/user/donate/` + paymentId, {
+			amount,
+			postId,
+			currOrgId,
+			userId,
+			paymentId,
+		});
+	},
 };
 
 export default CrowdfundingApi;
