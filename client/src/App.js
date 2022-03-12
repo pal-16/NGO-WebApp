27dcb421660978a2fce9a49622c73f8ebc968822
Archeel Parekh <archeelparekh11@gmail.com>
2022-03-12 18:10:32 +0530

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,15 @@ import { isLoggedIn } from "./utils/jwtUtil";
 import Header from "./components/Partials/Header";
 import Footer from "./components/Partials/Footer";
 import Loader from "./components/Loader/Loader";
-
 const Home = React.lazy(() => import("./pages/Home"));
-const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const Marketplace = React.lazy(() => import("./pages/Marketplace"));
 const Token = React.lazy(() => import("./pages/Token"));
 const Company = React.lazy(() => import("./pages/Company"));
 const Features = React.lazy(() => import("./pages/Features"));
 const MyToken = React.lazy(() => import("./pages/MyToken"));
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const Polls = React.lazy(() => import("./pages/Polls"));
+const Stat = React.lazy(() => import("./pages/Stat"));
 
 const App = () => {
 	const [isAuthenticated, setIsAuthenticated] = useState(isLoggedIn());
@@ -26,12 +28,13 @@ const App = () => {
 		} else {
 			setIsAuthenticated(false);
 			localStorage.removeItem("token");
+			localStorage.removeItem("user");
 		}
 	}, []);
 
 	return (
 		<Router>
-			<div className="App">
+			<div className="App" style={{ background: "white" }}>
 				<Header
 					isAuthenticated={isAuthenticated}
 					setIsAuthenticated={setIsAuthenticated}
@@ -50,12 +53,24 @@ const App = () => {
 						/>
 						<Route path="/company" exact element={<Company />} />
 						<Route path="/features" exact element={<Features />} />
-						<Route path="/dashboard" exact element={<Dashboard />} />
+						<Route path="/marketplace" exact element={<Marketplace />} />
 						<Route path="/marketplace/:tokenID" element={<Token />} />
 						<Route
 							path="/myToken"
 							element={isAuthenticated ? <MyToken /> : <Navigate to="/" />}
 						/>
+						<Route
+							path="/dashboard"
+							element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />}
+						>
+							<Route path="/dashboard/stat" exact element={<Stat />} />
+							<Route
+								path="/dashboard/polls/:tokenID"
+								exact
+								element={<Polls />}
+							/>
+							<Route path="/dashboard/MyToken" exact element={<MyToken />} />
+						</Route>
 					</Routes>
 				</Suspense>
 				<Footer />
